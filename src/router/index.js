af32e11b7b65965feb73e6952cd5fb13382efd07
sebbmn <sebbmn@gmail.com>
2017-07-04 21:10:20 +0200

    added MovieList.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/components/Home';
+import MovieList from '@/components/MovieList';
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
             path: '/',
             name: 'Home',
             component: Home
+        },
+        {
+            path: '/MovieList',
+            name: 'MovieList',
+            component: MovieList
         }
     ]
 });
