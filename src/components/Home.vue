646be0d47400ef279951269bebb83cafb6c87f5b
Francesco <francesc.amoroso@he-arc.ch>
2017-07-05 20:39:41 +0200

    sync

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -38,6 +38,7 @@
 
 <script>
 
+// import Search from './Search';
 const LOCALSTORAGE_KEY = 'vuedemo-malist';
 
 export default {
@@ -102,6 +103,6 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
 
 </style>
