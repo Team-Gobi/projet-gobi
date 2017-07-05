fd8fc4b5e037636aae95ab3c209994146e5af438
sebbmn <sebbmn@gmail.com>
2017-07-05 18:01:20 +0200

    sync

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,10 +1,11 @@
 <template>
     <div>
         <h1>{{ $route.params.query }}</h1>
+        <input v-model="searchRequest">
+        <button @click="displaySearch()">search</button>
         <p v-for="result in results" v-bind:src="result" v-bind:key="result">
             {{ result.title}}
             {{ result.release_date }}
-            <img href="https://amc.ig.he-arc.ch/tmdb/t/p/w500">
         </p>
     </div>
 </template>
@@ -13,6 +14,7 @@
 import axios from 'axios';
 
 export default {
+    name: 'search',
     data () {
         axios.get(`https://amc.ig.he-arc.ch/tmdb/search/movie?query=${this.$route.params.query}`)
         .then((response) => {
@@ -24,8 +26,15 @@ export default {
         });
 
         return {
+            searchRequest: 'ok ',
             results: []
         };
+    },
+    methods: {
+        displaySearch () {
+        },
+        addMovie (p) {
+        }
     }
 };
 </script>
