267599d969bc36d100cd641c67cb7796cd0ae97b
sebbmn <sebbmn@gmail.com>
2017-07-06 10:13:01 +0200

    new theme

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,40 +1,35 @@
 <template>
- 
-  <div class="home">
+    <div class="home">
+        <search></search>   
+        <md-list>
+            <md-list-item>
+            <h1 class="md-title"><strong>Mes Films</strong></h1>
+            </md-list-item>
+        </md-list>
+        <md-list>
+            <md-list-item>
+                <md-input-container>
+                    <label>Filtre</label>
+                    <md-input v-model="query"></md-input>
+                </md-input-container>
+            </md-list-item>
+        </md-list>
 
-   <search></search>
-
-   <md-list>
-    <md-list-item>
-      <md-input-container>
-        <label>Filtre</label>
-        <md-input v-model="query"></md-input>
-      </md-input-container>
-    </md-list-item>
-  </md-list>
-
-  <md-list>
-    <md-list-item>
-      <h1 class="md-title"><strong>Mes Films</strong></h1>
-    </md-list-item>
-  </md-list>
-
-  <md-list>
-      <md-list-item v-for="(movie, index) in listFiltre" v-bind:key="index">
-        <md-divider class="md-inset"></md-divider>
-        <md-avatar>
-          <img v-bind:src="movie | toFilmPoster" alt="Image">
-        </md-avatar>
-        <md-list-item>
-        <router-link v-bind:to="{path: '/movie', query: {index: movieList.indexOf(movie)}}"><span><b>{{ movie.title }}</b></span></router-link>
-        </md-list-item>
-        <md-button v-on:click="remove(movie)" class="md-icon-button">
-        <md-icon>remove</md-icon>
-        </md-button>
-      </md-list-item>
-  </md-list>
+        <md-list>
+            <md-list-item v-for="(movie, index) in listFiltre" v-bind:key="index">
+                <md-divider class="md-inset"></md-divider>
+                <md-avatar>
+                    <img v-bind:src="movie | toFilmPoster" alt="Image">
+                </md-avatar>
+                <md-list-item>
+                    <router-link v-bind:to="{path: '/movie', query: {index: movieList.indexOf(movie)}}"><span><b>{{ movie.title }}</b></span></router-link>
+                </md-list-item>
+                <md-button v-on:click="remove(movie)" class="md-icon-button">
+                    <md-icon>remove</md-icon>
+                </md-button>
+            </md-list-item>
+        </md-list>
   </div>
-
 </template>
 
 <script>
