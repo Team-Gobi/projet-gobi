b58c59d5133700ccb30d72a3b48a0ddf036d727e
Francesco <francesc.amoroso@he-arc.ch>
2017-07-06 10:49:21 +0200

    synchro

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,8 @@
       <!-- <img src="./assets/logo-gobi-sobre.png"> -->
 
   <md-toolbar class="md-toolbar-container">
-    <h2 class="md-title" style="flex: 1">GOBI
-      <md-icon>theaters</md-icon>
-    </h2>
+    <md-icon>theaters</md-icon>
+    <h2 class="md-title" style="flex: 1">GOBI</h2>
     <md-button class="md-icon-button">
       <a href="./"><md-icon class="md-size-1x md-accent">home</md-icon></a>
     </md-button>
