6e90089d31b4c7d131d795b97cf5e13cb4452678
Francesco <francesc.amoroso@he-arc.ch>
2017-07-06 13:50:56 +0200

    Correction du lien http en https dans search pour les images des films

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -67,7 +67,7 @@ export default {
     },
     filters: {
         toFilmPoster (movie) {
-            return `http://image.tmdb.org/t/p/w185${movie.poster_path}`;
+            return `https://image.tmdb.org/t/p/w185${movie.poster_path}`;
         }
     },
     watch: {
